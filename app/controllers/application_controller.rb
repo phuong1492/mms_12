1ea79cfeb5923aa38597b94a91fb8dd5ebadff77
phuong <phuong1492.bka@gmail.com>
2015-06-12 10:41:11 +0700

    Show all User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
   end
+
+  private
+  def after_sign_in_path_for resource
+    current_user.is_admin? ? admin_root_path : root_path
+  end
 end
