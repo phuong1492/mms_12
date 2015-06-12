4d4f746378971b227842defb28160c33f40a8284
phuong <phuong1492.bka@gmail.com>
2015-06-12 17:40:59 +0700

    sdsd

--- app/controllers/admin/positions_controller.rb
+++ app/controllers/admin/positions_controller.rb
@@ -7,7 +7,47 @@ class Admin::PositionsController < ApplicationController
     @positions = @positions.paginate page: params[:page], per_page: Settings.index.page
   end
 
+  def new
+    @position = Position.new
+  end
+
+  def create
+    @position = Position.new position_params
+    if @position.save
+      flash[:info] = "Created new position: #{@position.name}"
+      redirect_to admin_position_path(@position)
+    else
+      render :new
+    end
+  end
+
   def show
     @position = Position.find params[:id]
+  end
+
+  def edit
+    @position = Position.find params[:id]
+  end
+
+    def update
+    @position = Position.find params[:id]
+    if @position.update(position_params)
+      flash[:info] = "Updated Position!"
+      redirect_to admin_position_path(@position)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Position.find(params[:id]).destroy
+    flash[:success] = "Deleted position."
+    redirect_to admin_positions_path
+  end
+
+
+  private
+  def position_params
+    params.require(:position).permit :name, :abbreviation
   end
 end
