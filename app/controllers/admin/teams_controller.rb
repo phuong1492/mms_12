93576fbd0f835d4c200d6e3dd5a85e75c367873c
Hoang-Phuong <phuong1492.bka@gmail.com>
2015-06-25 20:17:30 +0700

    edit team

--- app/controllers/admin/teams_controller.rb
+++ app/controllers/admin/teams_controller.rb
@@ -12,7 +12,7 @@ class Admin::TeamsController < ApplicationController
 
   def new
     @team = Team.new
-    @leaders = User.user_not_in_team
+    @free_users = User.not_in_team
   end
 
   def create
@@ -26,6 +26,31 @@ class Admin::TeamsController < ApplicationController
     end
   end
 
+  def edit
+    @free_users = User.not_in_team
+    @users_in_team = @team.users.pluck :name, :id
+    @leaders = @free_users + @team.users
+  end
+
+  def update
+    if @team.update_attributes team_params
+      check_leader
+      flash[:success] = t "admin.team.success"
+      redirect_to admin_team_path @team
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @team.destroy
+      flash[:success] = t "admin.team.deleted_team"
+    else
+      flash[:fail] = t "admin.team.delete_fail" 
+    end 
+    redirect_to admin_teams_path
+  end
+
   private
   def set_team
     @team = Team.find params[:id]
@@ -34,5 +59,10 @@ class Admin::TeamsController < ApplicationController
   def team_params
     params.require(:team).permit :name, :description, :leader_id,
                                  :team_id, user_ids: []
+  end
+
+  def check_leader 
+    @team.users << @team.leader unless @team.users.
+      find_by id: team_params[:leader_id]
   end
 end
